70272dc83c54de1d05c5421143ca3dc18495c185
Jermaine Oppong <jeropp00@gmail.com>
2021-01-21 13:03:25 +0000

    Initial commit

---
+++ spa_server/bin/spa_server.dart
@@ -0,0 +1,5 @@
+import 'package:spa_server/spa_server.dart' as spa_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spa_server.calculate()}!');
+}
---
+++ spa_server/test/spa_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:spa_server/spa_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
